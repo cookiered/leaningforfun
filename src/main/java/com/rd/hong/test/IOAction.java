d81ce07c660569ecbfc7381d313df48d9d8877c4
hongbinglin <2990131904@qq.com>
2016-06-20 10:30:18 +0800

    [mod] 代码上传

---
+++ src/main/java/com/rd/hong/test/IOAction.java
@@ -0,0 +1,28 @@
+package com.rd.hong.test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+/**
+ * Created by hongbinglin on 2016/6/16.
+ */
+public class IOAction {
+    public void serve(int port) throws IOException{
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+        ServerSocket ssocket = serverChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        ssocket.bind(address);
+        Selector selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+        for(;;){
+
+        }
+    }
+}
